b6e79dab519e2a9bdc77e63ea16e7a51ae4ef625
Cloudreil <annaf5664@gmail.com>
2024-02-09 23:30:46 +0300

    Added Drawer, 2 scrollbars, Container 'Synopsis',edited Icon.buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,8 +13,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Hamilton Musical',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+        scaffoldBackgroundColor: Colors.black38,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amberAccent),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Hamilton App'),
@@ -35,46 +36,231 @@ class MyHomePage extends StatelessWidget {
             title: Center(
               child: Image.asset(
                 'assets/images/hamilton_logo.png',
-                height: 350,
-                width: 350,
+                height: 70,
               ),
             )
-        ),
-      body: Center(
-          child: Column(
-            children: [Container(
-              width: 350,
-              height: 100,
-              margin: const EdgeInsets.only(top:30),
+        ), //Аппбар
+      body: ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top:50, bottom: 60),
+                child: SizedBox(
+                height: 190,
 
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: const DecorationImage(
-                  image: AssetImage(
-                    'assets/images/gold_back.png',
-                  ),
-                  fit: BoxFit.fill,
-                ),
+                    child: Scrollbar(
+                        thumbVisibility: true,
+                        child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children:[
+                                Image.asset('assets/images/ham_1.png'),
+                                 Image.asset('assets/images/ham_2.png'),
+                                Image.asset('assets/images/ham_3.png')
+                            ],
+                        ),
+                        ),
 
-              ),
-              child: const Text('CAST',
-                  textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 60,
+              )
+            )
+            ),  //Первый скролл пикс(3 пикчи)
+              const Divider(
+              thickness: 5,
+              color: Color.fromRGBO(64, 64, 64, 100),
+            ),
+              const Padding(
+                 padding: EdgeInsets.only(top:40, bottom:30),
+                 child: Text("The real life of one of America's foremost founding fathers and first Secretary of the Treasury, Alexander Hamilton",
+                   textAlign: TextAlign.center,
+                   style: TextStyle(
                   fontFamily: 'CormorantSC',
-                  shadows: [
-                    Shadow(
-                      color: Colors.black,
-                    blurRadius: 10.0,
-                    )
-                  ]
-
+                  fontSize: 30,
+                  color: Colors.white,
+                  shadows: [Shadow(
+                    color: Colors.white,
+                    blurRadius: 50
+                  )]
                 ),
               ),
-            )],
-          )
-      )
+              ), //Текст с описанием
+              const Divider(
+                    thickness: 5,
+                    color: Color.fromRGBO(64, 64, 64, 100),
+                ),
+              Center(
+                child: Container(
+                  margin: const EdgeInsets.only(top:70, bottom: 70),
+
+                  height: 150,
+                  width: 400,
+
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: <Color>[
+                        Color.fromARGB(255, 255, 215, 115),
+                        Color.fromARGB(255, 255, 182, 0)
+                      ]
+                    ),
+                    image: DecorationImage(
+                        image: AssetImage(
+                            'assets/images/star.png',
+                        ),
+                      alignment: Alignment.bottomLeft,
+                      scale: 0.1
+                    )
+                  ),
+                  child: const Text(
+                    'Synopsis',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'CormorantSC',
+                        fontSize: 70,
+                        shadows: [Shadow(
+                          color: Colors.white,
+                          blurRadius: 20,
+                        )]
+                    ),
+                  ),
+              ),
+            ),  //Контейнер с градиентом
+              const Divider(
+                thickness: 5,
+                color: Color.fromRGBO(64, 64, 64, 100),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top:50, bottom:50),
+                
+                child: SizedBox(
+                  height: 195,
+                  
+                  child: Scrollbar(
+                    thumbVisibility: true,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          Image.asset('assets/images/ham_duel_1.png'),
+                          Image.asset('assets/images/ham_duel_2.png')
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),  // Второй скролл бар с пикчами(2 пикчи)
+              const Divider(
+                thickness: 5,
+                color: Color.fromRGBO(64, 64, 64, 100),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top:40, bottom:30),
+                child: Text("Hamilton narrates Alexander Hamilton's life in two acts, and details among other things his involvement in the American Revolutionary War as an aide-de-camp to George Washington, his marriage to Elizabeth Schuyler Hamilton, his career as a lawyer and secretary of the treasury, and his interactions with Aaron Burr which culminates in their duel at the end of Hamilton's life",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontFamily: 'CormorantSC',
+                      fontSize: 30,
+                      color: Colors.white,
+                      shadows: [Shadow(
+                          color: Colors.white,
+                          blurRadius: 50
+                      )]
+                  ),
+                ),
+              ),
+            ],
+      ),
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.amberAccent
+                ),
+                child: Text(
+                    'Menu',
+                  style: TextStyle(
+                    fontFamily: 'CormorantSC',
+                    fontSize: 40,
+                    shadows: [Shadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0
+                    )]
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                    'Home',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'CormorantSC',
+                    shadows: [Shadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0
+                    )]
+                  ),
+                ),
+                onTap: () {}),
+              ListTile(
+                title: const Text(
+                    'Info',
+                  style: TextStyle(
+                    fontFamily: 'CormorantSC',
+                    fontSize: 30,
+                    shadows: [Shadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0
+                    )]
+                  ),
+                ),
+                onTap: () {}
+              ),
+               ListTile(
+                title: const Text(
+                  'Tickets',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'CormorantSC',
+                    shadows: [Shadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0
+                    )]
+                  ),
+                ),
+                onTap: (){}
+              ),
+            ],
+          ),
+        ),
+
+
+
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.amberAccent,
+          height: 60,
+          child: Center(
+            child: IconButton(
+              onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                      content: Text(
+                          'I like dilfs',
+                      style:TextStyle(
+                        fontFamily: 'CormorantSC',
+                        fontSize: 20
+                      )
+                      )
+                  )
+              );
+              },
+              icon: const Icon(Icons.layers)
+            ),
+          ),
+        )
     );
   }
 }
